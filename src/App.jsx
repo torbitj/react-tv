b3d9d29eebb753b8a7a98dd644fcbf2a92f56c52
JT <jstorbit@gmail.com>
2025-12-04 13:31:24 -0700

    Add show selection component to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+import {useState} from "react"
+import { tvShows } from "./shows/data";
+import ShowSelection from "./shows/ShowSelection";
+
 export default function App() {
+  const [selectedShow, setSelectedShow] = useState({})
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection
+          shows={tvShows}
+          setSelectedShow={setSelectedShow}
+        />
       </header>
-      <main></main>
+      <main>
+        
+      </main>
     </>
   );
 }
