7ddd489b3ff5cf5459e11c95e3e285493e961a2c
JT <jstorbit@gmail.com>
2025-12-04 13:23:03 -0700

    Add show selection component to display show list and set selected show when clicked

--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,16 @@
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({shows, setSelectedShow}) {
+  return (
+    <nav className="shows">
+      {shows.map((show) => {
+        <a href="#" className="show" key={show.title} onClick={() => {
+          setSelectedShow(show);
+        }}>
+          {show.title}
+        </a>;
+      })}
+    </nav>
+  );
 }
